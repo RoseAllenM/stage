fc6ea3e34b7eb3745f0700d00c4aba9c2e0b74b5
Allen Rose <RoseAllenM@gmail.com>
2026-03-11 13:14:35 -0700

    Refactor onto Asset & Version classes

---
+++ src/stage/assets.py
@@ -0,0 +1,41 @@
+import logging
+from typing import TYPE_CHECKING
+
+import stage.entities
+from stage.versions import Version
+
+if TYPE_CHECKING:
+    from stage.resolver import Resolver
+
+
+logger = logging.getLogger(__name__)
+
+
+class Asset(stage.entities.Entity):
+    """A high-level production tracking element, a container for versioned work."""
+
+    _template = "asset"
+
+    def __init__(self, kind: str, name: str, resolver: Resolver | None = None):
+        self._tokens = {"kind": kind, "name": name}
+
+        super().__init__(resolver=resolver)
+
+        self._versions = None
+
+    def create(self, exist_ok: bool = False):
+        """Create the Asset on disk."""
+        self._path.mkdir(parents=True, exist_ok=exist_ok)
+
+    def new_version(self, department: str, number: int, active: bool) -> Version:
+        """Create a new Version of this Asset."""
+        version = Version(
+            asset=self,
+            department=department,
+            number=number,
+            resolver=self._resolver,
+        )
+
+        version.create(active=active)
+
+        return version
---
+++ src/stage/entities.py
@@ -0,0 +1,45 @@
+import logging
+
+from upath import UPath
+
+from stage.resolver import Resolver
+
+logger = logging.getLogger(__name__)
+
+
+class Entity:
+    """Base class for Database entries."""
+
+    _template = None
+
+    def __init__(self, resolver: Resolver | None = None):
+        self._resolver = resolver or Resolver()
+
+        if self._template is None:
+            raise NotImplementedError(
+                f"{self.__class__.__name__} doesn't have an associated Template",
+            )
+
+        self._id = self._resolver.resolve(self._template, **self.tokens)
+
+    def __repr__(self):
+        package = ".".join(self.__module__.split(".")[:2])
+        return f"<{package}.{self.__class__.__name__}: {self.id} at {hex(id(self))}>"
+
+    @property
+    def _path(self) -> UPath:
+        """The location on disk that defines this Entity."""
+        return self._resolver.store / self.id
+
+    @property
+    def id(self) -> str:
+        """The unique identier for this Entity."""
+        return self._id
+
+    @property
+    def tokens(self) -> dict[str, int | str]:
+        """The identifying key/values for this Entity."""
+        try:
+            return getattr(self, "_tokens")
+        except AttributeError:
+            return {}

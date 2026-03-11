8bf11ee7cdcda04a23e54b6b8440b80986041603
Allen Rose <RoseAllenM@gmail.com>
2026-03-11 10:41:13 -0700

    Allow configuration files from outside the repo

--- src/stage/resolver.py
+++ src/stage/resolver.py
@@ -1,62 +1,58 @@
+import importlib.resources
 import logging
 import os
 import re
 import tempfile
-from importlib.resources import files
 
 from omegaconf import OmegaConf
 from upath import UPath
 
 logger = logging.getLogger(__name__)
 
-_CONFIG = "stage._config"
-_STAGE_ROOT = "STAGE_ROOT"
+_DEFAULT_CONFIG = "stage._config"
+_STAGE_CONFIG = "STAGE_CONFIG"
+_STAGE_STORE = "STAGE_STORE"
 
 
 class Resolver:
     """Asset version validator and resolver."""
 
     def __init__(self) -> None:
-        self._root = None
+        self._store = None
+
         self._templates = None
         self._tokens = None
 
     @property
-    def root(self) -> UPath:
-        """The root directory of all other template paths."""
-        if self._root is None:
-            root = os.getenv(_STAGE_ROOT)
-            if root:
-                self._root = UPath(root)
+    def store(self) -> UPath:
+        """The root storage directory of all other template paths."""
+        if self._store is None:
+            store = os.getenv(_STAGE_STORE)
+            if store:
+                self._store = UPath(store)
             else:
-                self._root = UPath(tempfile.gettempdir()) / "stage"
+                self._store = UPath(tempfile.gettempdir()) / "stage"
                 logger.warning(
                     "'%s' environment variable is NOT defined, using %s",
-                    _STAGE_ROOT,
-                    self._root.as_posix(),
+                    _STAGE_STORE,
+                    self._store.as_posix(),
                 )
 
-        return self._root
+        return self._store
 
     @property
     def templates(self) -> dict[str, str]:
         """All named tokenized locations."""
         if self._templates is None:
-            self._templates = OmegaConf.to_container(
-                OmegaConf.load(str(files(_CONFIG).joinpath("templates.yaml"))),
-                resolve=True,
-            )
-        return self._templates  # pyright: ignore[reportReturnType]
+            self._templates = _load_config_file("templates.yaml")
+        return self._templates
 
     @property
     def tokens(self) -> dict[str, list[str] | str]:
         """The validation patterns for tokens in templates."""
         if self._tokens is None:
-            self._tokens = OmegaConf.to_container(
-                OmegaConf.load(str(files(_CONFIG).joinpath("tokens.yaml"))),
-                resolve=True,
-            )
-        return self._tokens  # pyright: ignore[reportReturnType]
+            self._tokens = _load_config_file("tokens.yaml")
+        return self._tokens
 
     def _validate_token(self, key: str, value: str):
         """Ensure the given value match the requirements for the token."""
@@ -100,8 +96,19 @@ class Resolver:
             self._validate_token(key, value)
 
         try:
-            return self.root / path.format(**tokens)
+            return self.store / path.format(**tokens)
         except KeyError as e:
             msg = f"Template {template} missing required tokens {path}"
             logger.warning(msg)
             raise KeyError(msg) from e
+
+
+def _load_config_file(name: str) -> dict:
+    """The named file from the configuration directory."""
+    path = os.getenv(_STAGE_CONFIG)
+    if path:
+        config = str(UPath(path) / name)
+    else:
+        config = str(importlib.resources.files(_DEFAULT_CONFIG).joinpath(name))
+
+    return OmegaConf.to_container(OmegaConf.load(config), resolve=True)  # pyright: ignore[reportReturnType]
